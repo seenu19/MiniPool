591407be857e6fb96bbf10b4c843841da8fd8443
rengelbert <rengelbert@gmail.com>
2013-01-13 18:15:04 -0200

    Initial Commit

---
+++ MiniPool/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  MiniPoolAppController.h
+//  MiniPool
+//
+//  Created by Roger Engelbert on 1/13/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ MiniPool/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  MiniPoolAppController.h
+//  MiniPool
+//
+//  Created by Roger Engelbert on 1/13/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
